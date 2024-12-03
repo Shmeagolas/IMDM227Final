c903cafb2c6634f206cac55be7cfc926169caa21
deerbo <ellagood10@yahoo.com>
2024-12-03 01:24:49 -0500

    Light Detection Obj + Script
    I used the code from seeside project to create a camera (which is under the floor for now but I will probably disable it later as I continue working) that will print to the debug console if the light is on or if the light is off in the room you are sitting in. As of now the value that determines if the lights are on or off needs to be changed manually.

---
+++ Assets/Scripts/LightDetection.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetection : MonoBehaviour
+{
+    
+    // public Transform ball;
+    const int waitWidth = 16;
+
+    public int width = 640;
+    public int height = 360;
+
+    Color32[] pixels;
+
+    WebCamTexture cam;
+    Texture2D tex;
+
+    Action update = () => { };
+    void Start()
+    {
+        update = WaitingForCam;
+
+        cam = new WebCamTexture(WebCamTexture.devices[0].name, width, height, 30);
+        cam.Play();
+        int w = cam.width;
+        Debug.Log($"Width = {width}, Height = {height}");
+    }
+    private void Update()
+    {
+        update();
+    }
+    void WaitingForCam()
+    {
+        if (cam.width > waitWidth)
+        {
+            width = cam.width;
+            height = cam.height;
+            Debug.Log($"Width = {width}, Height = {height}");
+            pixels = new Color32[cam.width * cam.height];
+            tex = new Texture2D(cam.width, cam.height, TextureFormat.RGBA32, false);
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = tex;
+            update = CamIsOn;
+        }
+        else
+        {
+            Debug.Log("Not Yet");
+        }
+    }
+    void CamIsOn()
+    {
+        if (cam.didUpdateThisFrame)
+        {
+            cam.GetPixels32(pixels);
+
+            // get total amount of light and print
+            float total = 0;
+            foreach (Color32 c in pixels)
+            {
+                total += c.r;
+                total += c.b;
+                total += c.g;
+            }
+            if (total > 9E+07) {
+                Debug.Log("the light is on: " + total);
+            } else {
+                Debug.Log("the light is off: " + total);
+            }
+
+            tex.SetPixels32(pixels);
+            tex.Apply();
+        }
+    }
+}
