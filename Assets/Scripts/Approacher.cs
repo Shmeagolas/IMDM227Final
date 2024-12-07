95897b9db3c2576ae2b341069716d6ac717c9490
christinakwonn <ckwon12@terpmail.umd.edu>
2024-12-07 02:48:55 -0500

    Fixed approacher
    Changed approacher to be more smooth when they get in the player's boundary and tried to limit stacking

--- Assets/Scripts/Approacher.cs
+++ Assets/Scripts/Approacher.cs
@@ -1,56 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Approacher : MonoBehaviour
-// {
-//    //[SerializeField] public GameObject player;
-//    public float approachSpeed;
-//    private float stuckDuration = 3f;
-
-//    //manually setting player position
-//    private Vector3 playerPosition = new Vector3(0, 1, -10);
-
-//    private Vector3 lastPosition;
-//    private float stuckTime = 0f;
-
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-
-//         if (transform.position == lastPosition) {
-//             stuckTime += Time.deltaTime;
-//             if (stuckTime >= stuckDuration) {
-//                 getUnstuck();
-//                 stuckTime = 0f;
-//             }
-//         } else {
-//             stuckTime = 0f;
-//         }
-
-//         lastPosition = transform.position;
-
-//         //making object move towards the playerPosition
-//         transform.position = Vector3.MoveTowards(transform.position, playerPosition, approachSpeed * Time.deltaTime);
-//     }
-
-//     void getUnstuck() {
-//         //if finds whether object is stuck to the left or right
-//         if (transform.position.x < playerPosition.x) {
-//             //move left
-//             transform.position += Vector3.left * 2f;
-//         } else {
-//             //move right
-//             transform.position += Vector3.right * 2f;
-//         }
-//     }
-// }
-
-
-
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -73,29 +20,23 @@ public class Approacher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float distanceToPlayer = Vector3.Distance(transform.position, playerPosition);
 
-        if (Vector3.Distance(transform.position, lastPosition) < positionRange)
-        {
+        if (Vector3.Distance(transform.position, lastPosition) < positionRange) {
             stuckTime += Time.deltaTime;
 
-            if (stuckTime >= stuckDuration)
-            {
+            if (stuckTime >= stuckDuration && distanceToPlayer > playerRing) {
                 getUnstuck();
                 stuckTime = 0f;
             }
-        }
-        else
-        {
+        } else {
             stuckTime = 0f;
         }
 
         lastPosition = transform.position;
 
  
-        if (distanceToPlayer > playerRing)
-        {
+        if (distanceToPlayer > playerRing) {
             transform.position = Vector3.MoveTowards(transform.position, playerPosition, approachSpeed * Time.deltaTime);
         }
     }
@@ -106,16 +47,17 @@ public class Approacher : MonoBehaviour
 
         Vector3 directionToPlayer = playerPosition - transform.position;
 
-        if (Vector3.Dot(transform.right, directionToPlayer) > 0)
-        {
-            // Object is on the right side of the player, move right
-            transform.position += transform.right * 0.5f;
+        Vector3 moveDirection;
+        if (Vector3.Dot(transform.right, directionToPlayer) > 0) {
+            //right
+            moveDirection = transform.right;
+        } else {
+            //left
+            moveDirection = -(transform.right);
         }
-        else
-        {
-            // Object is on the left side of the player, move left
-            transform.position -= transform.right * 0.5f;
-        }
+
+        Vector3 targetPos = transform.position + moveDirection * 1f;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, approachSpeed * Time.deltaTime);
     }
 }
 
